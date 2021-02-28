b6fa4820c68564bba1d402c3dac73cf6b0d6acd5
milkshake0 <jiwoo992020@gmail.com>
2021-03-01 04:35:47 +0900

    Profile 컴포넌트 mock.json 데이터 연결

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      try {
+        await axios.get("http://localhost:4000/user").then((response) => {
+          console.log(response.data);
+          setUser(response.data);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <div>대기 중...</div>;
+  }
+
+  if (!user) {
+    return null;
+  }
   return (
     <div className="Profile">
-      <a href="#" className="image" rel="noopener noreferrer">
-        <img
-          src="http://eifow.cafe24.com/reactpractice1/img/thumb.png"
-          alt="프로필 이미지"
-        />
+      <a href={user.url} className="image" rel="noopener noreferrer">
+        <img src={user.thumbUrl} alt="프로필 이미지" />
       </a>
       <div className="text">
-        <p className="text-name">Username</p>
-        <p className="text-desc">DescriptionDescriptionDescription</p>
+        <a href={user.url} className="text-name" rel="noopener noreferrer">
+          {user.username}
+        </a>
+        <p className="text-desc">{user.description}</p>
       </div>
     </div>
   );
